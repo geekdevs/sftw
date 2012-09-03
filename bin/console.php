58f7e2fb66eab9e5bc243f44cdae723d0bc43352
David Weinraub <david@papayasoft.com>
2012-09-03 10:24:25 +0700

    Again, some idiot can't even spell.

--- bin/console.php
+++ bin/console.php
@@ -7,5 +7,5 @@ use Dws\Console\Command\Stfw as StfwCommand;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $application = new Console\Application('Demo', '1.0.0');
-$application->add(new StfwCommand('stfw'));
+$application->add(new StfwCommand('sftw'));
 $application->run();
