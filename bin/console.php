726a9e5fd59b31dbbc011a711831ec6734490871
David Weinraub <david@papayasoft.com>
2012-09-04 18:21:01 +0700

    Smarten up the autoload include

--- bin/console.php
+++ bin/console.php
@@ -4,7 +4,18 @@
 use Symfony\Component\Console;
 use Dws\Console\Command\Sftw as SftwCommand;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+function includeIfExists($file)
+{
+    if (file_exists($file)) {
+        return include $file;
+    }
+}
+
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
 
 $application = new Console\Application('Demo', '1.0.0');
 $application->add(new SftwCommand('sftw'));
